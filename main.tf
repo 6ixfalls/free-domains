2e81ee253cd8db7265962142ade74a3f812d3a76
Six <23470032+6ixfalls@users.noreply.github.com>
2023-12-14 18:35:14 -0800

    test fix

--- main.tf
+++ main.tf
@@ -9,7 +9,10 @@ terraform {
       version = "4.20.0"
     }
   }
-  backend "s3" {}
+  backend "s3" {
+    skip_credentials_validation = true
+    skip_metadata_api_check = true
+  }
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
